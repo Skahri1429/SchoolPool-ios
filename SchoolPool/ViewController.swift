2be6ace0f023f632d3e7366e50ccec43bbe3465d
Sarthak Khillon <sarthakkhillon24@gmail.com>
2016-01-31 17:23:30 -0800

    Initial Commit

---
+++ SchoolPool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SchoolPool
+//
+//  Created by Pankaj Khillon on 1/31/16.
+//  Copyright © 2016 Sarthak Khillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
